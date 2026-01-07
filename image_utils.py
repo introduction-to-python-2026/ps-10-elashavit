435232efa1f72dedb1edcd17821a3bc479b75d6e
elashavit <ela.shavit@gmail.com>
2026-01-07 13:43:51 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -3,7 +3,27 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+    ` img = Image.open(path).convert("RGB")
+    return np.array(img)
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+    gray = image.mean(axis=2)
+
+    kernelY = np.array([
+        [1, 0, -1],
+        [2, 0, -2],
+        [1, 0, -1]
+    ])
+
+    kernelX = np.array([
+        [-1, -2, -1],
+        [0,  0,  0],
+        [1,  2,  1]
+    ])
+
+    edgeX = convolve2d(gray, kernelX, mode="same", boundary="fill", fillvalue=0)
+    edgeY = convolve2d(gray, kernelY, mode="same", boundary="fill", fillvalue=0)
+
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+
+    return edgeMAG
